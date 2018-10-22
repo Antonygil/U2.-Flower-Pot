b45f2e2afaf4cbffbcfeb1ba4af836244688b85b
JULIOALPUCHE <julio_alpuche1996@hotmail.com>
2018-10-22 09:20:41 -0500

    second

--- 4. Executable files/first.c
+++ 4. Executable files/first.c
@@ -2,7 +2,6 @@
 #device ADC=10       //Resolución de 10 bits para ADC
 #fuses INTRC_IO,NOCPD,NOWDT,NOPUT,NOLVP,NOBROWNOUT,MCLR
 #use delay(clock=2M)
-#define use_portd_lcd TRUE
 #use rs232(baud=9600,xmit=PIN_C6,rcv=PIN_C7) // Pines para establecer comunicación RS232
 #include <lcd.c>
 
@@ -26,3 +25,47 @@ delay_us(10);                    // tiempo de espera de 10 us segundos
 conversion=read_adc();           //conversión lee el canal analógico
 set_adc_channel(segundo);     // definiendo el canal que se va a leer
 delay_us(10); // tiempo de espera de 10 us segundos
+
+conversion1=read_adc()  ;   //Conversión1 lee el canal analóico
+if(conversion>conversion1)   //Condisión conversión> conversió 1
+{
+temp=conversion-conversion1;
+
+temo=temp*500/1023 ;          //rectifica el sensor de temperatura
+delay_ms(200); //tiempo de espera de 200 msSegundos
+}
+if(temo>37)   // consicion de rangos de temp si temo menor a treinta
+{
+OUTPUT_high(PIN_C3);  // nivel alto pin C3
+delay_ms(30);         //espera 50 ms S
+OUTPUT_low(PIN_C3);   //apaga C3
+delay_ms(20);        //espera 50 ms S
+OUTPUT_high(PIN_C3); //nivel alto pin C3
+delay_ms(20);           //espera 50 ms S
+OUTPUT_low(PIN_C3);   //apaga C3
+delay_ms(20);           //espera 50 ms S
+OUTPUT_high(PIN_C3);    // nivel alto pin C3
+delay_ms(20);           //espera 50 ms S
+OUTPUT_low(PIN_C3);   //apaga C3
+delay_ms(20);           //espera 50 ms S
+OUTPUT_high(PIN_C3);    // nivel alto pin C3
+delay_ms(20);           //espera 50 ms S
+OUTPUT_low(PIN_C3);   //apaga C3
+delay_ms(20);           //espera 50 ms S
+OUTPUT_high(PIN_C3);    // nivel alto pin C3
+OUTPUT_low(PIN_C4);         //apaga C4
+OUTPUT_low(PIN_C5); //apaga C5
+
+}
+
+if(temo>30 && temo<37 )       //temo esta entre 10 y 30
+{
+OUTPUT_high(PIN_C4);        //prende CA
+delay_ms(100);             //espera 100 ms
+OUTPUT_low(PIN_C4);          //apaga C3
+delay_ms(100);               //espera 100 ms S
+OUTPUT_high(PIN_C4);          //prende C4
+delay_ms(100);                //espera 100 ms
+OUTPUT_low(PIN_C3);        //apaga C3
+OUTPUT_low(PIN_C5);           //apaga C5
+}
